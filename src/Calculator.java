bf13c68568794b3e34bd65c61d8d134ddf3f091e
MrRamonBenitez <mr.ramon.benitez@gmail.com git config --global user.email mr.ramon.benitez@gmail.com git config --global user.email mr.ramon.benitez@gmail.com>
2021-12-25 16:34:59 +0500

    .gitignore modyfied

---
+++ src/Calculator.java
@@ -0,0 +1,23 @@
+import java.util.function.*;
+
+public class Calculator {
+
+    public static double NaN = Double.longBitsToDouble(0x7ff8000000000000L);
+
+    static Supplier instance = Calculator::new;
+
+    Consumer<Integer> println = System.out::println;
+    Consumer<String> printer = System.out::println;
+
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    // BinaryOperator<Integer> devide = (x, y) -> x / y; - начальный вариант
+    BinaryOperator<Double> devide = (x, y) -> y != 0 ? x / y : NaN;
+
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+
+    Predicate<Integer> isPositive = x -> x > 0;
+
+}
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1 , 1);
+        // int c = calc.devide.apply(a, b); при указанных выше значениях аргументов при выполнении операции деления
+        //                                  произойдет деление на ноль и, соответственно, будет выброшено исключение
+        //                                  ArithmeticException
+        double c = calc.devide.apply((double)a, (double)b);
+
+        if (c != calc.NaN)
+            calc.println.accept((int) c);
+        else
+            calc.printer.accept("Деление на ноль!");
+
+    }
+
+}
